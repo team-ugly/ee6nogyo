ab27f26ea47d0bb45da7d224685010e0e15fd84f
ishimaru-school <maru.gooooooo@gmail.com>
2017-10-06 15:29:09 +0900

    first

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    added_attrs = [ :username, :email, :password, :password_confirmation　]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,19 @@
+module ApplicationHelper
+  # IDEの補完機能を有効にするため、Deviseのヘルパを宣言
+  def authenticate_user!(opts={})
+    opts[:scope] = :user
+    warden.authenticate!(opts) if !devise_controller? || opts.delete(:force)
+  end
+
+  def user_signed_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= warden.authenticate(:scope => :user)
+  end
+
+  def user_session
+    current_user && warden.session(:user)
+  end
+end
